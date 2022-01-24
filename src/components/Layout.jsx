cfb30ce42b54f89fbe4504e2c19caf3a58c68217
Andre Rodrigues <67346660+andre-rd-rodrigues@users.noreply.github.com>
2022-01-24 13:11:46 +0000

    Added contact page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { ToastContainer } from "react-toastify";
 
 function Layout({ children }) {
   return (
@@ -8,6 +9,18 @@ function Layout({ children }) {
       <Navbar />
       {children}
       <Footer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        progressClassName="progressBar"
+      />
     </div>
   );
 }
